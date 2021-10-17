146af7ed4ddc143affe10ca1cd3db2999495b07d
suabae <bsa0322@ewhain.net>
2021-10-17 21:54:51 +0900

    [DFS & BFS] 10월 17일 - update

--- 10월 15일 - DFS와 BFS/라이브 코딩/7576.cpp
+++ 10월 15일 - DFS와 BFS/라이브 코딩/7576.cpp
@@ -11,8 +11,8 @@ typedef pair<int, int> ci; //row, col 저장하기 위한 pair
 
 int bfs(int n, int m, int cnt, vector<vector<int>> &matrix, queue<ci> q) {
     //상, 하, 좌, 우
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
+    int dr[4] = {-1, 1, 0, 0};
+    int dc[4] = {0, 0, -1, 1};
 
     int t = 0;
     while (!q.empty()) {
@@ -22,8 +22,8 @@ int bfs(int n, int m, int cnt, vector<vector<int>> &matrix, queue<ci> q) {
         t = matrix[cr][cc]; //현재 탐색까지 걸린 시간
         cnt--; //익은 토마토 감소
         for (int i = 0; i < 4; i++) { //상, 하, 좌, 우 방향 탐색(인접한 노드 탐색)
-            int nr = cr + dx[i]; //방문하려는 노드
-            int nc = cc + dy[i]; //방문하려는 노드
+            int nr = cr + dr[i]; //방문하려는 노드
+            int nc = cc + dc[i]; //방문하려는 노드
             if (nr > 0 && nr <= n && nc > 0 && nc <= m && matrix[nr][nc] == 0) { //노드가 존재(행렬 범위 안에 있음), 익지 않은 토마토라면
                 matrix[nr][nc] = matrix[cr][cc] + 1; //시간 증가값을 토마토 행렬에 저장
                 q.push(ci(nr, nc));
